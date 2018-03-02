08521c6909ba79c6f3fff27686ba882fd5ab397f
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-03-02 00:14:46 -0500

    Update Feb-8-2018.c

--- Code-By-Language/C/Feb-8-2018.c
+++ Code-By-Language/C/Feb-8-2018.c
@@ -1,8 +1,8 @@
-//************************
-//*  2. Add Two Numbers  *
-//************************
+//****************
+//*  1. Two Sum  *
+//****************
 
-//O(n) solution
+//Incomplete solution
 typedef struct Node
 {
   int val;
