39bbda83bb82fbfba0d247eb0b0d7683a03149f0
Jeremy Banks <31806672+JeremyPBanks@users.noreply.github.com>
2018-03-11 22:51:31 -0400

    Add files via upload

---
+++ 2018/March/March-11-2018.c
@@ -0,0 +1,22 @@
+//***************************
+//*  191. Number of 1 Bits  *
+//***************************
+
+//O(1) solution [Because n is a 32-bit integer, and that's what is looped through]
+int hammingWeight(uint32_t n) 
+{
+    int count = 0;
+    uint32_t check = 0;
+    
+    while (n > 0)
+    {
+        check = n & 1;
+        if (check == 1)
+        {
+            count += 1;
+        }
+        n >>= 1;
+    }
+    
+    return count;
+}
